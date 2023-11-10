cb9c2e971d27f63adb6c3a87afe38b19c85389bf
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-10 09:23:06 -0500

    ajout formulaire sur signUp

--- pages/signUp.php
+++ pages/signUp.php
@@ -1,2 +1,14 @@
 <h2>Enregistrement</h2>
-<a href="../index.php">Accueil</a
+<a href="../index.php">Accueil</a>
+<form method="post" action="../results/signUpResult.php">
+<label for="user_name">Nom d'utilisateur</label>
+<input type="text" name="user_name" id="user_name" value="">
+
+<label for="email"> Email d'utilisateur</label>
+<input type="text" name="email" id="email" value="">
+
+<label for="pwd">Mot de passe d'utilisateur</label>
+<input type="text" name="pwd" id="pwd" value="">
+
+<button type ="submit">Soumettre mon Enregistrement</button>
+</form>
