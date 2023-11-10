9c161a82d34a043f6085b860fe2cf82cfea575cd
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2023-11-10 09:22:27 -0500

    ajout formulaire sur login

--- pages/login.php
+++ pages/login.php
@@ -1,2 +1,15 @@
 <h2>Connexion</h2>
 <a href="../index.php">Accueil</a>
+
+<form method="post" action="../results/loginResult.php">
+
+<label for="user_name">Nom d'utilisateur</label>
+<input type="text" name="user_name" id="user_name" value=""><br><br>
+
+
+
+<label for="pwd">Mot de passe d'utilisateur</label>
+<input type="text" name="pwd" id="pwd" value=""><br><br>
+
+<button type ="submit">Soumettre sa connexion</button>
+</form>
